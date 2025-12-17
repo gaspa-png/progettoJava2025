d808120760ba51282c753305d966769207a39741
gaspa712 <ger720902@gmail.com>
2025-12-17 09:10:53 +0100

    progettoJava2025

--- src/main/java/progettoJava2025/Start.java
+++ src/main/java/progettoJava2025/Start.java
@@ -2,9 +2,9 @@ package progettoJava2025;
 
 public class Start {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("ciao");
 	}
 
 }
